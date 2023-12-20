946f1519a9cf19986708b11aa567081e9fb80ae7
guteswetter <simon.erni@students.bfh.ch>
2023-12-20 21:34:12 +0100

    timeline hinzugefügt, ein paar sachen kaputt gemacht

--- script.js
+++ script.js
@@ -32,12 +32,16 @@ gsap.from(".experiences", {
   x: "-100vw",
 });
 
+/* Navigation */
+
 const menuToggle = document.getElementById("mobile-menu");
 const navList = document.querySelector(".nav-list");
 
 menuToggle.addEventListener("click", () => {
   navList.classList.toggle("active");
 });
+
+/* Skill bars */
 
 function animateSkills() {
   const skills = document.querySelectorAll(".skill");
@@ -48,7 +52,7 @@ function animateSkills() {
     bar.style.width = `${value}%`;
 
     const logo = skill.querySelector(".logo");
-    logo.style.left = `calc(${value}% - 30px)`; // Adjust the value based on logo width
+    logo.style.left = `calc(${value}% - 25px)`; // Adjust the value based on logo width
   });
 }
 
@@ -64,4 +68,26 @@ function checkVisibility() {
   }
 }
 
+/* Timeline */
+
 window.addEventListener("scroll", checkVisibility);
+
+const stations = document.querySelectorAll(".station");
+
+function checkInView() {
+  stations.forEach((station) => {
+    const rect = station.getBoundingClientRect();
+    const isInView = rect.top >= 0 && rect.bottom <= window.innerHeight;
+    if (isInView) {
+      station.classList.add("active");
+    } else {
+      station.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkInView);
+window.addEventListener("resize", checkInView);
+
+// Check initial view
+checkInView();
